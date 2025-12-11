c29d526ba8068631ea48ccfb6baadff49c35470e
horizon-spark <166980733+horizon-spark@users.noreply.github.com>
2025-12-11 16:01:25 +0300

    DataAnalysis: passed lw6, continued lw7

--- Assembler/Function/function.asm
+++ Assembler/Function/function.asm
@@ -2,60 +2,54 @@ section .text
 
 global _start
 
+_func:    
+    mov  rax, 0b0000000011111111
+    mov  rbx, 0b0000111100001111
+    mov  rcx, 0b0011001100110011
+    mov rdx, 0b0101010101010101
+
+    push  rax
+    OR  rax,  rbx
+    NOT  rcx
+    OR  rax,  rcx
+    NOT  rcx
+    NOT rdx
+    OR  rax, rdx
+    NOT rdx
+    push  rax
+    mov  rax, [rsp + 8]
+    
+    NOT  rbx
+    OR  rax,  rbx
+    NOT  rbx
+    OR  rax,  rcx
+    NOT rdx
+    OR  rax, rdx
+    NOT rdx
+    push  rax
+    mov  rax, [rsp + 16]
+    
+    NOT  rax
+    NOT  rbx
+    OR  rax,  rbx
+    NOT  rbx
+    OR  rax,  rcx
+    NOT rdx
+    OR  rax, rdx
+    NOT rdx
+    push  rax
+    mov  rax, [rsp + 24]	
+
+    pop  rcx
+    pop  rbx
+    pop  rax
+    pop  rdx
+    AND  rax,  rbx
+    AND  rax,  rcx
+   
+    ret
+
 _start:
-
-    mov ax, 0
-    mov bx, 0
-    mov cx, 0
-    mov dx, 0
-
-    call F
-
-F:
-    mov ax, 0b0000000011111111
-    mov bx, 0b0000111100001111
-    mov cx, 0b0011001100110011
-    mov dx, 0b0101010101010101
-    
-    push ax
-    OR ax, bx
-    NOT cx
-    OR ax, cx
-    NOT cx
-    NOT dx
-    OR ax, dx
-    NOT dx
-    push ax
-    mov ax, [rsp + 4]
-    
-    NOT bx
-    OR ax, bx
-    NOT bx
-    OR ax, cx
-    NOT dx
-    OR ax, dx
-    NOT dx
-    push ax
-    mov ax, [rsp + 8]
-    
-    NOT ax
-    NOT bx
-    OR ax, bx
-    NOT bx
-    OR ax, cx
-    NOT dx
-    OR ax, dx
-    NOT dx
-    push ax
-    mov ax, [rsp + 12]	
-
-    pop cx
-    pop bx
-    pop ax
-    AND ax, bx
-    AND ax, cx
-
-    mov bx, 0
-    mov cx, 0
-    mov dx, 0
+    call _func
+    ret
 	
